4f33a5f17751db429122b8d9385c9520762ec24f
Dan Moran <danmoran@broadinstitute.org>
2018-12-11 10:11:45 -0500

    Add endpoint marks to slider.

--- encode/explorer/ui/src/components/facets/FacetSlider.js
+++ encode/explorer/ui/src/components/facets/FacetSlider.js
@@ -6,7 +6,7 @@ import "rc-slider/assets/index.css";
 const styles = {
   facetSlider: {
     width: 400,
-    margin: 50
+    margin: 75
   }
 };
 
@@ -19,6 +19,9 @@ class FacetSlider extends React.Component {
 
     this.effectiveMin = Math.floor(props.min / this.step) * this.step;
     this.effectiveMax = Math.ceil(props.max / this.step) * this.step;
+    this.marks = {};
+    this.marks[this.effectiveMin] = this.effectiveMin;
+    this.marks[this.effectiveMax] = this.effectiveMax;
 
     this.state = {
       low: this.effectiveMin,
@@ -46,6 +49,7 @@ class FacetSlider extends React.Component {
           allowCross={false}
           onChange={this.onChange}
           onAfterChange={this.saveChange}
+          marks={this.marks}
         />
       </div>
     );
