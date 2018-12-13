6656050482e306aebb29bc83f1bc117794a57ca0
Daniel Moran <danxmoran@gmail.com>
2018-12-13 13:38:07 -0500

    DSPDC-87 Add config / logging for DB connection leaks (#37)

--- encode/explorer/src/main/scala/org/broadinstitute/gdr/encode/explorer/db/DbConfig.scala
+++ encode/explorer/src/main/scala/org/broadinstitute/gdr/encode/explorer/db/DbConfig.scala
@@ -3,12 +3,15 @@ package org.broadinstitute.gdr.encode.explorer.db
 import pureconfig.ConfigReader
 import pureconfig.generic.semiauto._
 
+import scala.concurrent.duration.FiniteDuration
+
 /** Settings for connecting to a database. */
 case class DbConfig(
   driverClassname: String,
   connectURL: String,
   username: String,
-  password: String
+  password: String,
+  leakDetectionTimeout: FiniteDuration
 )
 
 object DbConfig {
