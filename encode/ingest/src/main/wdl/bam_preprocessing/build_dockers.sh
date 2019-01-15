03f33c3b160647b015819416b26ff6749dd844cb
benjamincarlin <bcarlin@broadinstitute.org>
2019-01-15 18:10:19 -0500

    create bam index and gsutil cp

---
+++ encode/ingest/src/main/wdl/bam_preprocessing/build_dockers.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# versions of tools used
+declare -r INDEX_BAMS_VERSION=1.0
+declare -r SAMTOOLS_VERSION=1.9
+
+function build_docker () {
+  # using the tool/dir name
+  local -r image_tag=$1
+  # get the version of the tool/docker
+  local -r image_version=$(echo $2 | cut -d"=" -f2)
+  # get the relative path to the docker
+  local -r dockerfile_path="docker/${image_tag}/Dockerfile"
+  # get the images name
+  #local -r image_name="quay.io/broadinstitute/${image_tag}:${image_version}"
+  local -r image_name="quay.io/bcarlin/test:latest"
+  # get additional arguments with the "--build-arg" option
+  local -a build_args=()
+  for arg in $@; do
+    build_args=(--build-arg ${arg} ${build_args[@]})
+  done
+
+  # build and push the docker
+  docker build ${build_args[@]} -t ${image_name} -f ${dockerfile_path} .
+  docker push ${image_name}
+}
+
+# build dockers (dir/tool name must be 1st arg & version name must be 2nd arg)
+build_docker index_bams INDEX_BAMS_VERSION=${INDEX_BAMS_VERSION} SAMTOOLS_VERSION=${SAMTOOLS_VERSION}
+
